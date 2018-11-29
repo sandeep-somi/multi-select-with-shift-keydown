d726587886b3a12c732e8fee3ad8d6f56e4cc1c8
Sandeep Rajput <soami.ssr@gmail.com>
2018-11-29 19:25:17 +0530

    added stop propagation and drop down close on click outside

--- src/App.js
+++ src/App.js
@@ -28,35 +28,48 @@ class App extends Component {
     console.log(i, 'onDelete');
   }
 
+  hideDropdown = (e) => {
+    //e.stopPropagation();
+    if(this.state.open) {
+      this.setState({
+        open: false
+      })
+    }
+  }
+
   render() {
     const { data } = this.state;
     return (
-      <div className="select-container">
-        <div className="select-bar" onClick={() => this.setState({ open: !this.state.open })}>
-          {data && data.map(i => <React.Fragment><span className="itm">{i} <span className="close-icon" onClick={this.onDelete.bind(this, i)}>x</span></span></React.Fragment>)}
+      <div className="container" onClick={this.hideDropdown}>
+        <div className="select-container">
+          <div className="select-bar" onClick={() => this.setState({ open: !this.state.open })}>
+            {data && data.map(i => <React.Fragment><span className="itm">{i} <span className="close-icon" onClick={this.onDelete.bind(this, i)}>x</span></span></React.Fragment>)}
+          </div>
+          <div onClick={(e) => e.stopPropagation()}>
+            {this.state.open ?<React.Fragment>
+              <form onSubmit={this.onSubmit}>
+              <select
+                multiple
+                id="fruits"
+                className="custom-mulit-select"
+                onChange={this.handleSelect}
+              >
+                <option value="apple">Apple</option>
+                <option value="banana">Banana</option>
+                <option value="mango">Mango</option>
+                <option value="grape">Grape</option>
+                <option value="watermelon">watermelon</option>
+                <option value="Apricot">Apricot</option>
+                <option value="Peach">Peach</option>
+                <option value="lemon">lemon</option>
+                <option value="Chilly">Chilly</option>
+              </select>
+              <input type="submit" style={{ display: 'none' }}></input>
+              </form>
+            </React.Fragment>
+            : null }
+          </div>
         </div>
-        {this.state.open ?<React.Fragment>
-          <form onSubmit={this.onSubmit}>
-          <select
-            multiple
-            id="fruits"
-            className="custom-mulit-select"
-            onChange={this.handleSelect}
-          >
-            <option value="apple">Apple</option>
-            <option value="banana">Banana</option>
-            <option value="mango">Mango</option>
-            <option value="grape">Grape</option>
-            <option value="watermelon">watermelon</option>
-            <option value="Apricot">Apricot</option>
-            <option value="Peach">Peach</option>
-            <option value="lemon">lemon</option>
-            <option value="Chilly">Chilly</option>
-          </select>
-          <input type="submit" style={{ display: 'none' }}></input>
-          </form>
-        </React.Fragment>
-         : null }
       </div>
     );
   }
